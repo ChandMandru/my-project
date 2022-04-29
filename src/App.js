4ff7c28ea82d2706836916ca2cb1d16e9182fd48
Chand Mandru <chand.mandru@student.hs-rm.de>
2022-04-29 21:47:32 +0200

    Custom Hook, useState und useEffect Cleanups, ErrorHandling WIP, UI WIP

--- src/App.js
+++ src/App.js
@@ -1,53 +1,84 @@
-import rooms from "./data"
-import {RoomCard,HotelQuery} from "./Hotel Components/Hotel"
-
+import { RoomCard, HotelQuery } from "./Hotel Components/Hotel";
+import { useState } from "react";
 
 const App = () => {
   return (
     <>
-    <div className="App">
-      <Header />
-      <Body rooms={rooms} />
-      <Footer />
-    </div>
+      <div className="App min-h-full">
+        <Header />
+        <Body />
+        <Footer />
+      </div>
     </>
   );
 };
 
 const Header = () => {
-  return(
-    <>
-    <h1>LovelyHotel.com</h1>
-    <hr/>
-    </>
-    ); 
-};
-
-
-const Body = ({ rooms }) => {
   return (
     <>
-    <div className="grid grid-cols-2">
-      <div >
-      <h2>These Rooms are Available</h2>
-        {rooms.map((room) => (
-          <RoomCard key={room.name} room={room} />
-        ))}
-      </div>
-      <HotelQuery />
-    </div>
+      <h1 className="text-[35px] text-center font-mono p-4 ">
+        LovelyHotel.com
+      </h1>
+      <hr />
     </>
   );
 };
 
+const Body = () => {
+  const [rooms, setRooms] = useState([]);
+  const [filter, setFilter] = useState({
+    adult: "",
+    child: "",
+    duration: "",
+    arrival: "",
+  });
+
+  return (
+    <>
+      <div className="grid grid-cols-2 pt-6 gap-5 ml-20 mr-20">
+        <div className="border-2 rounded-md">
+          <h2 className="text-[25px] font-bold text-center font-mono pb-7">
+            These Rooms are Available
+          </h2>
+          <div className="grid grid-cols-2 min-w-auto">
+            {rooms &&
+              rooms.map((room) => (
+                <RoomCard
+                  key={room.name}
+                  room={room}
+                  duration={filter.duration}
+                />
+              ))}
+          </div>
+        </div>
+        <HotelQuery setRooms={setRooms} filter={filter} setFilter={setFilter} />
+      </div>
+    </>
+  );
+};
 
 const Footer = () => {
   return (
     <>
-    <div>
-      <div >Info + Contact</div>
-      <div >Copyright</div>
-    </div>
+      <footer className="absolute inset-x-0 bottom-0 grid grid-cols-2 p-6 ml-20 mr-20 min-w-auto bg-blue-700 text-white">
+        <div className="text-left p-3">
+          We hope that you will enjoy your stay at our hotel! <br />
+          if you have any questions or concerns <br />
+          just give us a call <br />
+          0172 34589 <br />
+          or{" "}
+          <a
+            className="underline"
+            href="https://www.google.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            write us
+          </a>{" "}
+          <br />
+        </div>
+        <p className="place-self-end">Copyright 2022 LovelyHotel.com</p>
+      </footer>
     </>
   );
 };
--- src/Hotel Components/Hotel.js
+++ src/Hotel Components/Hotel.js
@@ -1,73 +1,138 @@
 import HotelPicture from "../images/hotel.jpg";
-import "../index.css"
+import "../index.css";
+import { useRoomData } from "./useRoomData";
+import { useEffect, useState } from "react";
 
-
-
-const RoomCard = ({ room }) => {
+const RoomCard = ({ room, duration }) => {
   const people = getPeople(room.beds);
 
   return (
     <>
-      <div className="grid grid-cols-2 content-center">
-        <img
-          className="rounded-md max-h-[80%] max-w-[80%]"
-          src={HotelPicture}
-          alt="Hotel Room"
-        />
+      <div className="grid grid-cols-2 p-3 gap-4 max-w-md max-h-md">
+        <img className="rounded-md" src={HotelPicture} alt="Hotel Room" />
         <div>
           <b>{room.name}</b>
           <p>Adults : {people.adults}</p>
           <p>Kids : {people.children}</p>
           <p>Price : {room.price}/night</p>
           <p>Availability : {room.amount}</p>
+          <p>{duration && `Total Price = ${duration * room.price}`}</p>
         </div>
       </div>
     </>
   );
 };
 
-const HotelQuery = () => {
-  function handleSubmit(e) {
-    getHotel();
-    e.preventDefault();
+const HotelQuery = ({ setRooms, filter, setFilter }) => {
+  const [clicked, setClicked] = useState();
+
+  const hotelRoom = useRoomData(filter, clicked);
+  const { data, loading, error } = hotelRoom;
+
+  useEffect(() => {
+    setRooms(data);
+    console.table(data);
+  }, [data, setRooms]);
+
+  if (loading) {
+    return <div>Loading data...</div>;
+  }
+
+  if (error) {
+    alert(
+      "Error - Errorcode [ " +
+        error +
+        " ]\n The Request Failed ... \n Please Try again in a couple of Seconds !"
+    );
   }
 
   return (
     <>
-      <div className="border-2 p-5 rounded-md container max-w-[500px] h-fit">
+      <div className="border-2 p-5 rounded-md max-w-md min-h-full place-self-end">
         <form
           name="HotelSearch"
-          onSubmit={handleSubmit}
+          onSubmit={(e) => {
+            setClicked((prev) => !prev);
+            e.preventDefault();
+          }}
         >
-          <div className="grid grid-cols-2 grid-rows-2 place-items-center pt-3">
-
-
-            <div >
+          <div className="grid grid-cols-2 grid-rows-2 gap-4 pt-3">
+            <div>
               <label htmlFor="arrival">Arrival</label>
               <br />
-              <input className="border-2 rounded-md" type="text" id="arrival" name="arrival" />
+              <input
+                className="border-2 rounded-md"
+                type="text"
+                id="arrival"
+                name="arrival"
+                onChange={(e) =>
+                  setFilter((prevState) => ({
+                    ...prevState,
+                    arrival: e.target.value,
+                  }))
+                }
+                value={filter.arrival}
+              />
             </div>
             <div>
               <label htmlFor="adult">Num. Of Adults</label>
               <br />
-              <input className="border-2 rounded-md" type="text" id="adult" name="adult" />
+              <input
+                className="border-2 rounded-md"
+                type="text"
+                id="adult"
+                name="adult"
+                onChange={(e) =>
+                  setFilter((prevState) => ({
+                    ...prevState,
+                    adult: e.target.value,
+                  }))
+                }
+                value={filter.adult}
+              />
             </div>
-            <div >
+            <div>
               <label htmlFor="child">Num. Of Children</label>
               <br />
-              <input className="border-2 rounded-md" type="text" id="child" name="child" />
+              <input
+                className="border-2 rounded-md"
+                type="text"
+                id="child"
+                name="child "
+                onChange={(e) =>
+                  setFilter((prevState) => ({
+                    ...prevState,
+                    child: e.target.value,
+                  }))
+                }
+                value={filter.child}
+              />
             </div>
 
-            <div >
+            <div>
               <label htmlFor="duration">Duration</label>
               <br />
-              <input className="border-2 rounded-md" type="text" id="duration" name="duration" />
+              <input
+                className="border-2 rounded-md"
+                type="text"
+                id="duration"
+                name="duration"
+                onChange={(e) =>
+                  setFilter((prevState) => ({
+                    ...prevState,
+                    duration: e.target.value,
+                  }))
+                }
+                value={filter.duration}
+              />
             </div>
-
-
           </div>
-          <div className="pt-5 pb-5 flex flex-row text-center justify-center">
-            <input className="w-full border-2 p-1 shadow-lg rounded-lg bg-sky-500 text-white" type="submit" value="Show Availability" />
+          <div className="pt-5 pb-5 flex flex-row text-center">
+            <input
+              className="w-full border-2 p-1 shadow-lg rounded-lg bg-sky-500 text-white"
+              type="submit"
+              value="Show Availability"
+            />
           </div>
         </form>
         <div>
@@ -99,42 +164,5 @@ const getPeople = (beds) => {
 
   return result;
 };
-
-async function getHotel() {
-  try {
-    const adult = document.forms["HotelSearch"]["adult"].value;
-    const child = document.forms["HotelSearch"]["child"].value;
-    const duration = document.forms["HotelSearch"]["duration"].value;
-
-    const response = await fetch(
-      "https://hsrm-hotel-api.herokuapp.com/rooms?" +
-      new URLSearchParams({ adults: adult, children: child })
-    );
-    const json = await response.json();
-    const divElement = document.getElementById("result");
-    let content = "";
-
-    for (const key of Object.keys(json)) {
-      content += `${json[key].amount}x ${json[key].name}, ${json[key].price
-        }/Night Total Cost: ${json[key].price * duration}\n`;
-    }
-
-    const res2 = content.split(/[\r\n]+/);
-    let counter = 1;
-
-    divElement.replaceChildren();
-
-    res2.forEach((item) => {
-      if (item !== "") {
-        const newDiv = document.createElement("div");
-        newDiv.appendChild(document.createTextNode(item));
-        divElement.appendChild(newDiv);
-        newDiv.setAttribute("id", "result" + counter++);
-      }
-    });
-  } catch (error) {
-    console.log("The Request Failed");
-  }
-}
 
 export { RoomCard, HotelQuery };
